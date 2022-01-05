68f3383257f96304dae767a21d904a211ede984a
Utkarsh Vishwakarma <avackrock60@gmail.com>
2022-01-05 15:32:30 +0530

    Cheching in array list

---
+++ ArrayAndArraylist/src/com/company/aL.java
@@ -0,0 +1,19 @@
+package com.company;
+import java.util.ArrayList;
+class aL {
+    public static void main(String[] args) {
+        ArrayList random = new ArrayList();
+        //Declare and create an object of the ArrayList class named 'random', in place of the line above
+
+        //Add the first four elements to this 'random' ArrayList
+
+        random.add(99);                    //Command for adding a new element
+        printArray(random);
+    }
+
+    public static void printArray(ArrayList arr) {   //Change the type of the parameter to ArrayList
+        for(Object o : random) {              //Change the data type of 'a' from 'int' to 'Object'
+            System.out.println(a);
+        }
+    }
+}
